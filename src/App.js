1849e9f68bd09ba48c2b41e81ae1fe4881365856
Peyton Moss <pmoss94@gmail.com>
2022-11-11 13:50:55 -0700

    form added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+    </div>
+  );
 }
 
 export default App;
