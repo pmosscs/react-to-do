41756d07620d4fc7a4d06ce53f98a91e94ac4da0
Peyton Moss <pmoss94@gmail.com>
2022-11-11 13:45:35 -0700

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h2>Peyton's To-Do List</h2>
+            
+        </header>
+    )
+}
+
+export default Header;
